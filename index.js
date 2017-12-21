b79b963aac1d227ced44af591d33c47f7aa4c83c
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-21 19:15:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers, string){
-      return   drivers.filter(function (string) { return string; })
+      return   drivers.filter(function(string) { return string; })
   //[1, 2, 3, 4, 5].filter(function (num) { return num > 3; });
 }
