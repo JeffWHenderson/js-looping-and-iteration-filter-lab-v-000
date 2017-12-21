0324dda59ee44367ab3df816472c4a90776e9b2f
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-12-21 19:30:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function findMatching(drivers, string){
 }
 
 function fuzzyMatch(drivers, string) {
-  //[1, 2, 3, 4, 5].filter(function (num) { return num > 3; })
   return drivers.filter(function(i){
     return i.slice(0, string.length) === string
   })
